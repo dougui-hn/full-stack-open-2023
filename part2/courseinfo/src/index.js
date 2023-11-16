370c5509a156e207c87cda38ef4a34c0467e087b
dougui-hn <douglas.guillen@gmail.com>
2023-11-16 08:21:50 -0600

    phonebook - initial

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,11 +1,11 @@
-import {React, StrictMode} from "react";
-import {createRoot} from "react-dom/client";
-import App from "./App";
+import { React, StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-const root = createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById('root'))
 root.render(
 	<StrictMode>
 		<h1>Web development curriculum</h1>
 		<App />
 	</StrictMode>
-);
+)
